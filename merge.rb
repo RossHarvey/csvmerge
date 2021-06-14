c2fa987feac4ef82657001d70ffc80ea46096f1a
Ross Harvey <chaos@ross.aero>
2021-06-14 14:59:33 -0700

    rh - fwv

--- merge.rb
+++ merge.rb
@@ -1,38 +1,45 @@
 require 'rubygems'
-require 'charlock_holmes'
 require 'csv'
-require 'pry'
 
 class Transpose
 
-  ENCODING = 'windows-1252'
-  READMODE = 'r:' + ENCODING
+  ENCODING  = 'windows-1252'
+  READMODE  = 'r:' + ENCODING
+  WRITEMODE = 'w:' + ENCODING
+  IDIR      = 'db2/'
 
   def run
     @uniq_fields = {}
     @field_index = {}
     CSV.open "r/fieldsurvey.csv", "w" do |csv|
-        # first scan all the field names in all dbs
-        ARGV.each do |file|
-          File.open file, READMODE do |f|
-            t = [file, *track(file, (CSV.parse f.gets).first)]
-            csv << t # + Array.new(73 - t.size)
+      ARGV.each do |file|
+        File.open file, READMODE do |f1|
+          File.open IDIR + file, WRITEMODE do |f2|
+            f2.puts f1.gets.gsub(' ,', ',')
+            while s = f1.gets
+              f2.puts s
+            end
           end
         end
-        puts '-' * 80
-        # Now merge
+      end
+      ARGV.each do |file|
+        File.open IDIR + file, READMODE do |f|
+          t = [file, *track(file, (CSV.parse f.gets).first)]
+          csv << t
+        end
+      end
+      puts '-' * 80
       CSV.open "r/merged_db.csv",
                "w",
                :write_headers => true,
                :headers       => @field_index.keys do |mcsv|
         ARGV.each do |file|
-          csv = CSV.parse(File.read(file, :encoding => ENCODING), headers: true)
-          # current_fields = (CSV.parse f.gets).first
-          # while s = f.gets
+          csv = CSV.parse(File.read(IDIR + file, :encoding => ENCODING), headers: true)
           csv.each do |row|
             newrecord = []
             row.each do |(key, value)|
-              newrecord[@field_index[key]] = value
+              p [file, key, value, @field_index[key]]
+              newrecord[@field_index[key]] = value if key && value
             end
             mcsv << newrecord
           end
@@ -48,16 +55,22 @@ class Transpose
   end
 
   # Compile a unique list of all fields. Depends on stable hash order.
-
   def track file, fields
-    fields.each do |seen_yet|
-      f = @uniq_fields[seen_yet]
-      if f
-        puts 'In "%s" field "%s" reoccurs' % [file, f]
-      else
-        @field_index[seen_yet] = @field_index.size
-        @uniq_fields[seen_yet] = file
-        puts 'In "%s" field "%s" originates' % [file, f]
+    p '>>>'
+    p file
+    p fields
+    fields.each do |hcn| # header column name
+      raise if hcn == ''
+      if hcn # ,, can produce a nil field
+        ocn = @field_index[hcn] # original column name
+        ofn = @uniq_fields[hcn] # original file name
+        if ocn
+          puts 'In "%s" field "%s" reoccurs' % [file, hcn]
+        else
+          @field_index[hcn] = @field_index.size
+          @uniq_fields[hcn] = file
+          puts 'In "%s" field "%s" originates' % [file, ocn]
+        end
       end
     end
     fields
