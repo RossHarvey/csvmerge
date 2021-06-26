3e648143dbd54f8b88d5b96199bc37dd25db15e2
Ross Harvey <chaos@ross.aero>
2021-06-25 21:09:46 -0700

    WIP, reasonably complete version.
    Now removes 8-bit characters. I didn't find a code-page in which they make
    sense, they may represent damage from prior misinterpretation. They aren't
    meaningful UTF-8, at least in some cases.
    
    Conversions for non-header lines extracted into method.
    Some names made more meaningful.

--- merge.rb
+++ merge.rb
@@ -3,55 +3,70 @@ require 'csv'
 
 class Transpose
 
-  ENCODING  = 'windows-1252'
-  READMODE  = 'r:' + ENCODING
-  WRITEMODE = 'w:' + ENCODING
+  READMODE  = 'r:UTF-8'
+  WRITEMODE = 'w:UTF-8'
+# WRITEMODE = 'w:windows-1252'
   IDIR      = 'db2/'
+  ENCODING  = 'UTF-8'
 
   def run
     @uniq_fields = {}
     @field_index = {}
-    CSV.open "r/fieldsurvey.csv", "w" do |csv|
+    # TODO: if the occasional 8-bit chars are not noise,
+    #       further analyze encoding
+    CSV.open "r/fieldsurvey.csv", "w" do |survey|
+      # read every file in binary mode, filter 8-bit characters,
+      # trailing commas, and spaces before commas in headers.
+      # encoding is not UTF-8 but also not an obvious codepage,
+      # leading to core errors when trying to encode as UTF-8
       ARGV.each do |file|
-        File.open file, READMODE do |f1|
+        puts file
+        bytes8bit = IO.binread file
+        StringIO.open bytes8bit do |f1|
           File.open IDIR + file, WRITEMODE do |f2|
-            f2.puts f1.gets.gsub(' ,', ',')
+            f2.puts trim f1.gets.gsub(' ,', ',')
             while s = f1.gets
-              f2.puts s
+              f2.puts trim s
             end
           end
         end
       end
+      # read each file again and build up the overall field list
       ARGV.each do |file|
         File.open IDIR + file, READMODE do |f|
           t = [file, *track(file, (CSV.parse f.gets).first)]
-          csv << t
+          survey << t
         end
       end
       puts '-' * 80
+      # read each file yet again and merge
       CSV.open "r/merged_db.csv",
                "w",
                :write_headers => true,
                :headers       => @field_index.keys do |mcsv|
         ARGV.each do |file|
+          puts file
           csv = CSV.parse(File.read(IDIR + file, :encoding => ENCODING), headers: true)
           csv.each do |row|
             newrecord = []
             row.each do |(key, value)|
-              p [file, key, value, @field_index[key]]
               newrecord[@field_index[key]] = value if key && value
             end
             mcsv << newrecord
           end
         end
       end
+      puts '%d uniq fields' % @uniq_fields.size
+      format = "%40s | %-40s"
+      puts format % ["<- FIELD -<", ">- FIRST SEEN IN ->"]
+#     [@uniq_fields.keys.to_a, @uniq_fields.values.to_a].transpose.each do |row|
+#       puts(format % row)
+#     end
     end
-    puts '%d uniq fields' % @uniq_fields.size
-    format = "%40s | %-40s"
-    puts format % ["<- FIELD -<", ">- FIRST SEEN IN ->"]
-    [@uniq_fields.keys.to_a, @uniq_fields.values.to_a].transpose.each do |row|
-      puts format % row
-    end
+  end
+
+  def trim s
+    s.gsub(/[^[:ascii:]]/, '').strip.gsub(/,,*$/,'')
   end
 
   # Compile a unique list of all fields. Depends on stable hash order.
