85ef3865584929e3b7988f6f7bb2a88e8e4d10f5
Ross Harvey <chaos@ross.aero>
2021-06-27 23:37:12 -0700

    rh - top level shell script

---
+++ runmerge.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+ruby "$@" ./merge.rb \
+  "dborig/U.S. Community Newspapers.csv" \
+  "dborig/U.S. Non-Daily Newspapers.csv" \
+  "dborig/College and University Newspapers.csv" \
+  "dborig/U.S. Daily Newspapers.csv" \
+  "dborig/Canadian Community Newspapers.csv" \
+  "dborig/Canadian Non-Daily Newspapers.csv" \
+  "dborig/Equipment, Supply and Service Companies.csv" \
+  "dborig/News, Picture and Syndicate Services.csv" \
+  "dborig/Canadian Daily Newspapers.csv" \
+  "dborig/U.S. Shopper Publications.csv" \
+  "dborig/U.S. Schools and Departments of Journalism.csv" \
+  "dborig/Interactive Products and Services Companies.csv" \
+  "dborig/College and University Journalism Departments.csv" \
+  "dborig/Professional, Business and Special Services for Dailies.csv" \
+  "dborig/U.S. Hispanic Newspapers.csv" \
+  "dborig/U.S. Black Newspapers.csv" \
+  "dborig/Associations, Clubs and Press Clubs - National and International.csv" \
+  "dborig/Associations, Clubs and Press Clubs - City, State and Regional.csv" \
+  "dborig/U.S. Ethnic Newspapers.csv" \
+  "dborig/U.S. Religious Publications.csv" \
+  "dborig/U.S. Alternative Newspapers.csv" \
+  "dborig/U.S. Jewish Newspapers.csv" \
+  "dborig/U.S. Parenting Publications.csv" \
+  "dborig/U.S. Military Newspapers.csv" \
+  "dborig/Alternate Delivery Services.csv" \
+  "dborig/Canadian Shopper Publications.csv" \
+  "dborig/U.S. Top 50 Newspaper Groups.csv" \
+  "dborig/U.S. State Newspaper Associations.csv" \
+  "dborig/U.S. Online Only Newspapers.csv" \
+  "dborig/U.S. Real Estate Publications.csv" \
+  "dborig/Newspaper Representatives - State.csv" \
+  "dborig/U.S. Senior Publications.csv" \
+  "dborig/Newspaper Representatives - National.csv" \
+  "dborig/Newspaper Brokers and Appraisers.csv" \
+  "dborig/Newspaper Distributed Magazines and TMC Publications.csv" \
+  "dborig/Clipping Bureaus.csv" \
+  "dborig/U.S. Gay and Lesbian Newspapers.csv" \
+  "dborig/Canadian Ethnic Newspapers.csv" \
+  "dborig/Canadian Newspaper Associations.csv" \
+  "dborig/Canadian Weekly-Shopper-TMC Newspaper Groups.csv" \
+  "dborig/Advertising, Circulation and Newspaper Promotion Services.csv" \
+  "dborig/Newspaper Representatives - Foreign.csv" \
+  "dborig/Canadian Alternative Newspapers.csv" \
+  "dborig/Electronic Clipping Bureaus.csv" \
+  "dborig/Canadian Newspaper Groups.csv" \
+  "dborig/Circulation Audit Services.csv" \
+  "dborig/Newspaper Trade Unions.csv" \
+  "dborig/Newspaper Comics Section Groups and Networks.csv"
