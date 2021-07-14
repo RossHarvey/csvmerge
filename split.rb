4feaa4dfbd578f26a02331b1a3b3db4b4670bf32
Ross Harvey <chaos@ross.aero>
2021-07-13 17:35:33 -0700

    reformat, report # of collisions

--- split.rb
+++ split.rb
@@ -13,20 +13,28 @@ class Split
     puts 'Clearing cols/'
     system 'rm -rf cols; mkdir cols'
     puts 'Reading merged_db.csv...'
+    collisions = 0
+
     table = CSV.read 'r/merged_db.csv', :headers=>true
+
     table.headers.each_with_index do |colname, i|
       puts "Writing #{colname} index=#{i}"
       ofn = 'cols/' + (table.headers[i].tr ' /', '_:')
       if test 'e', ofn
         puts; puts
         puts "#{colname} already exists, probably a case-folding collision"
+        puts
         system "grep -i '#{colname}' r/headers"
-        puts; puts
+        puts
+        collisions += 1
       end
       File.open ofn, 'w' do |f|
         f.puts table.by_col[colname]
       end
     end
+
+    puts "#{collisions} collisions"
+
   end
 
   self
